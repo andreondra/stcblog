6978b98709afe98575db5cadccf9a799ea6d7389
Ondřej Golasowski <golasowski.o@gmail.com>
2019-12-14 15:39:23 +0100

    Added page.php

---
+++ page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory');?>/style.css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title><?php echo get_bloginfo('name'); ?> <?php wp_title('| ', true,'left'); ?></title>
+        <?php wp_head(); ?>
+    </head>
+    <body class="ms-Fabric">
+        <?php get_header(); ?>
+               
+        <main class="main post">
+            <header class="main__header post__header">
+                <h1><?php the_post(); the_title(); ?></h1>
+            </header> 
+
+            <article class="post__content">
+                <?php the_content(); ?>
+            </article>
+        </main>
+
+        <?php get_footer(); ?>
+    </body>
+</html>
