eea21fbb008a74a41a06a19a1be4956c9d010e84
Ondrej Golasowski <golasowski.o@gmail.com>
2020-03-12 13:25:45 +0100

    Added margins to TOC.

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ add_theme_support( 'post-thumbnails' );
  * @author Ondrej Golasowski
  */
 function get_css_name(){
-    $version = 19;
+    $version = 20;
     return "/style.css?rnd=" . $version;
 }
 
