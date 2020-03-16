f67757c7cbc6bac523b981a072ccd06a63e570ef
petrkucerak <petr@khome.cz>
2020-03-16 07:03:30 +0100

    upload style of related posts

--- single.php
+++ single.php
@@ -38,6 +38,32 @@
                         <p class="post__footer__author__data__description"><?php the_author_meta('description'); ?> </p>
                     </div>
                 </section>
+                <section class="post__footer__recomendedPosts feed">
+                    <h2>Další články</h2>
+                    <?php $post_tags = get_the_tags();?>
+                    <?php $args = array(
+                        'tag' => $post_tags
+                    );?>
+                    <?php $the_query = new WP_Query( $args ); ?>
+                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                        <article>
+                        <a class="excerpt" href="<?php the_permalink(); ?>">
+                            <div class="excerpt__thumbnail">
+                                <?php the_post_thumbnail([300, 200]); ?>
+                            </div>
+                            <div class="excerpt__text">
+                                <h2 class="excerpt__text__title"><?php the_title(); ?></h2>
+                                <?php the_excerpt(); ?>
+                                <div class="excerpt__text__meta">
+                                    <span class="excerpt__text__meta__date"><?php echo get_the_date(); ?></span>
+                                    <span>•</span>
+                                    <span class="excerpt__text__meta__readtime">Doba čtení: <?php echo reading_time(); ?></span>
+                                </div>
+                            </div>
+                        </a>    
+                    </article>
+                    <?php endwhile; wp_reset_postdata(); ?>
+                </section>
             </footer>  
         </main>
 
