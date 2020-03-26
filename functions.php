e0492d5d4eaf8a9fb79fdc68497b43f7fbc820d5
Ondrej Golasowski <golasowski.o@gmail.com>
2020-03-26 18:13:32 +0100

    Tuned TOC styles.

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ add_theme_support( 'post-thumbnails' );
  * @author Ondrej Golasowski
  */
 function get_css_name(){
-    $version = 22;
+    $version = 24;
     return "/style.css?rnd=" . $version;
 }
 
