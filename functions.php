cb711cc6eeedf4564de342e3a80fb964fd01c84d
petrkucerak <petr@khome.cz>
2020-04-07 13:09:53 +0200

    add multiple authors

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ add_theme_support( 'post-thumbnails' );
  * @author Ondrej Golasowski
  */
 function get_css_name(){
-    $version = 26;
+    $version = 27;
     return "/style.css?rnd=" . $version;
 }
 
