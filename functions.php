9e090928cc4bb8eae9fb2a258a6f97f0af570cdc
petrkucerak <petr@khome.cz>
2020-04-21 09:19:08 +0200

    Edit authors profiles in posts

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ add_theme_support( 'post-thumbnails' );
  * @author Ondrej Golasowski
  */
 function get_css_name(){
-    $version = 27;
+    $version = 28;
     return "/style.css?rnd=" . $version;
 }
 
