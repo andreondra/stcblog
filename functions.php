6f004f6f9d1d5b5ccf3ba4c2ab8527e7a74e6bde
petrkucerak <petr@khome.cz>
2020-05-05 22:21:58 +0200

    change css vesrion

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ add_theme_support( 'post-thumbnails' );
  * @author Ondrej Golasowski
  */
 function get_css_name(){
-    $version = 29;
+    $version = 30;
     return "/style.css?rnd=" . $version;
 }
 
