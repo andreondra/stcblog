cba4f99b8fddbfc316e61a13e11564a1c84e4206
petrkucerak <petr@khome.cz>
2020-05-15 20:36:27 +0200

    add style of embedde wp post

---
+++ embed-content.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+<link rel="stylesheet" href="<?php echo get_bloginfo('template_directory') . get_css_name();?>"/>
+	<div <?php post_class( 'wp-embed' ); ?>>
+            <article class="wp-embed__posts">
+                <a target="_blank" class="excerpts" href="<?php the_permalink(); ?>">
+                    <div class="excerpts__thumbnail">
+                        <?php the_post_thumbnail([360, 240]); ?>
+                    </div>
+                    <div class="excerpts__text">
+						<h2 class="excerpts__text__title"><?php the_title(); ?></h2>
+						<?php 
+						function custom_excerpt_length() {
+							return 20;
+						}
+						add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+						the_excerpt(); ?>
+                        <div class="excerpts__text__meta">
+                            <span class="excerpts__text__meta__date"><?php echo get_the_date(); ?></span>
+                            <span>•</span>
+                            <span class="excerpts__text__meta__readtime">Doba čtení: <?php echo reading_time(); ?></span>
+                        </div>
+                    </div>
+                </a>
+            </article>
+	</div>
+<?php
