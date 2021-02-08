857566d1c748b32dfe599eef076fbb08ecaa251e
Matyáš Koc <matyas.koc@studentstc.cz>
2021-02-08 20:43:28 +0100

    Remove Google Tag Manager

--- footer.php
+++ footer.php
@@ -48,17 +48,6 @@
 
   gtag('config', 'UA-154876830-1');
 </script>
-<!-- Google Tag Manager -->
-<script type="text/plain" class="ce-script">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5XFHF3M');</script>
-<!-- End Google Tag Manager -->
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XFHF3M"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 <!-- Facebook Pixel Code -->
 <script type="text/plain" class="ce-script">
   !function(f,b,e,v,n,t,s)
--- functions.php
+++ functions.php
@@ -2,53 +2,59 @@
 
 /**
  * Special PHP functions.
+ *
  * @copyright Copyright (C) 2020 Ondrej Golasowski, Petr Kucera, Matyas Koc and contributors
- * @license GPL(GPLv3)
+ * @license   GPL(GPLv3)
  */
 
 /**
  * Remove empty paragraphs created by wpautop()
+ *
  * @author Ryan Hamilton
- * @link https://gist.github.com/Fantikerz/5557617
+ * @link   https://gist.github.com/Fantikerz/5557617
  */
-function remove_empty_p( $content ) {
-	$content = force_balance_tags( $content );
-	$content = preg_replace( '#<p>\s*+(<br\s*/*>)?\s*</p>#i', '', $content );
-	$content = preg_replace( '~\s?<p>(\s|&nbsp;)+</p>\s?~', '', $content );
-	return $content;
+function remove_empty_p($content) {
+    $content = force_balance_tags($content);
+    $content = preg_replace('#<p>\s*+(<br\s*/*>)?\s*</p>#i', '', $content);
+    $content = preg_replace('~\s?<p>(\s|&nbsp;)+</p>\s?~', '', $content);
+    return $content;
 }
 
 //Attach filter to the contents of articles.
 add_filter('the_content', 'remove_empty_p', 0, 1);
 //Allow post thumbnails.
-add_theme_support( 'post-thumbnails' );
+add_theme_support('post-thumbnails');
 
 /**
  * Attach a version suffix to the url of the style.css to force browsers to refresh CSS.
+ *
  * @author Ondrej Golasowski
  */
-function get_css_name(){
+function get_css_name() {
     $version = 34;
     return "/style.css?rnd=" . $version;
 }
 
 /**
  * Attach a version suffix to the url of the main.js to force browsers to refresh JS.
+ *
  * @author Ondrej Golasowski
  */
-function get_js_name(){
+function get_js_name() {
     $version = 3;
     return "/assets/js/main.js?rnd=" . $version;
 }
 
 /**
- * Create multiple socials icons. 
+ * Create multiple socials icons.
+ *
  * @param int Count of icons.
+ *
  * @author Ondrej Golasowski
  */
-function stcblog_add_socials($count, $customizer){
+function stcblog_add_socials($count, $customizer) {
 
-    for($id = 1; $id <= $count; $id++){
+    for ($id = 1; $id <= $count; $id++) {
 
         // Adding a template setting for a logo.
         $customizer->add_setting('settings_footer_logo' . $id, [
@@ -68,7 +74,7 @@ function stcblog_add_socials($count, $customizer){
 
         //Adding an url input control to the Customizer.
         $customizer->add_control(new WP_Customize_Control($customizer, 'control_footer_link' . $id, [
-            'label' => __( 'Socials ' . $id . ' - link', 'stcblog' ),
+            'label' => __('Socials ' . $id . ' - link', 'stcblog'),
             'section' => 'section_footer',
             'settings' => 'settings_footer_link' . $id,
             'type' => 'url'
@@ -78,40 +84,42 @@ function stcblog_add_socials($count, $customizer){
 
 /**
  * Add GDPR link to footer.
+ *
  * @author Ondrej Golasowski
  */
-function stcblog_add_gdpr($customizer){
+function stcblog_add_gdpr($customizer) {
 
-        // Adding a template setting for a text.
-        $customizer->add_setting('settings_footer_gdpr_text', [
-            'default' => ''
-        ]);
-        // Adding a template setting for a link.
-        $customizer->add_setting('settings_footer_gdpr_link', [
-            'default' => ''
-        ]);
+    // Adding a template setting for a text.
+    $customizer->add_setting('settings_footer_gdpr_text', [
+        'default' => ''
+    ]);
+    // Adding a template setting for a link.
+    $customizer->add_setting('settings_footer_gdpr_link', [
+        'default' => ''
+    ]);
 
-        //Adding an text input upload control to the Customizer.
-        $customizer->add_control(new WP_Customize_Control($customizer, 'control_footer_gdpr_text', [
-            'label' => __('GDPR text', 'stcblog'),
-            'section' => 'section_footer',
-            'settings' => 'settings_footer_gdpr_text'
-        ]));
+    //Adding an text input upload control to the Customizer.
+    $customizer->add_control(new WP_Customize_Control($customizer, 'control_footer_gdpr_text', [
+        'label' => __('GDPR text', 'stcblog'),
+        'section' => 'section_footer',
+        'settings' => 'settings_footer_gdpr_text'
+    ]));
 
-        //Adding an url input control to the Customizer.
-        $customizer->add_control(new WP_Customize_Control($customizer, 'control_footer_gdpr_link', [
-            'label' => __('GDPR link', 'stcblog' ),
-            'section' => 'section_footer',
-            'settings' => 'settings_footer_gdpr_link',
-            'type' => 'url'
-        ]));
+    //Adding an url input control to the Customizer.
+    $customizer->add_control(new WP_Customize_Control($customizer, 'control_footer_gdpr_link', [
+        'label' => __('GDPR link', 'stcblog'),
+        'section' => 'section_footer',
+        'settings' => 'settings_footer_gdpr_link',
+        'type' => 'url'
+    ]));
 }
 
 /**
  * Add MarCom mail to footer.
+ *
  * @author Petr Kučera
  */
-function stcblog_add_MarCommail($customizer){
+function stcblog_add_MarCommail($customizer) {
 
     //Adding a template setting for a MarCom mail.
     $customizer->add_setting('settings_footer_MarCommail', [
@@ -122,73 +130,75 @@ function stcblog_add_MarCommail($customizer){
     $customizer->add_control(new WP_Customize_Control($customizer, 'settings_footer_MarCommail', [
         'label' => __('MarCom mail', 'stcblog'),
         'section' => 'section_footer',
-        'description'=> 'Contact email for MarCom AG.',
+        'description' => 'Contact email for MarCom AG.',
         'settings' => 'settings_footer_MarCommail'
     ]));
 }
 
 //Adding functions to the Customizer.
-function stcblog_customize_register($wp_customize){
+function stcblog_customize_register($wp_customize) {
 
     // ***********************************
     // Footer section
     // ***********************************
     // Adding a section to the Customizer.
     $wp_customize->add_section('section_footer', [
-        'title' => __( 'Footer', 'stcblog' ),
+        'title' => __('Footer', 'stcblog'),
         'priority' => 30,
     ]);
-        // ===================================
-        // Custom copyright text
-        // ===================================
-        // Adding the template setting.
-        $wp_customize->add_setting('settings_footer_copyright', [
-            'default' => '© '.date("Y").' STC, Všechna práva vyhrazena'
-        ]);
+    // ===================================
+    // Custom copyright text
+    // ===================================
+    // Adding the template setting.
+    $wp_customize->add_setting('settings_footer_copyright', [
+        'default' => '© ' . date("Y") . ' STC, Všechna práva vyhrazena'
+    ]);
 
-        //Adding a control to the Customizer.
-        $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'control_footer_copyright', [
-            'label' => __( 'Copyright text', 'stcblog' ),
-            'section' => 'section_footer',
-            'settings' => 'settings_footer_copyright'
-        ]));
+    //Adding a control to the Customizer.
+    $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'control_footer_copyright', [
+        'label' => __('Copyright text', 'stcblog'),
+        'section' => 'section_footer',
+        'settings' => 'settings_footer_copyright'
+    ]));
 
-        // ===================================
-        // Custom GDPR link
-        // ===================================
-        stcblog_add_gdpr($wp_customize);
+    // ===================================
+    // Custom GDPR link
+    // ===================================
+    stcblog_add_gdpr($wp_customize);
 
-        // ===================================
-        // Custom MarCom mail
-        // ===================================
-        stcblog_add_MarCommail($wp_customize);
+    // ===================================
+    // Custom MarCom mail
+    // ===================================
+    stcblog_add_MarCommail($wp_customize);
 
-        // ===================================
-        // Custom socials icons
-        // ===================================
-        stcblog_add_socials(3, $wp_customize);
+    // ===================================
+    // Custom socials icons
+    // ===================================
+    stcblog_add_socials(3, $wp_customize);
 }
+
 add_action('customize_register', 'stcblog_customize_register');
 
 /**
  * Calculates reading time according to the article word count.
+ *
  * @return int Reading time.
  * @author Ondrej Golasowski
  */
 function reading_time() {
     //Get contents of the current article.
-    $content = get_post_field( 'post_content', $post->ID );
-    $word_count = str_word_count( strip_tags( $content ) );
+    $content = get_post_field('post_content', $post->ID);
+    $word_count = str_word_count(strip_tags($content));
     $readingtime = ceil($word_count / 200);
 
     //Chooses correct word for specified interval.
-    switch($readingtime){
+    switch ($readingtime) {
         case 1:
             $timer = " minuta";
             break;
         case 2:
         case 3:
-        case 4: 
+        case 4:
             $timer = " minuty";
             break;
         default:
@@ -203,6 +213,7 @@ function reading_time() {
 
 /**
  * Exporting posts to csv file
+ *
  * @author Petr Kučera
  */
 /*
@@ -250,24 +261,26 @@ function func_export_all_posts() {
 */
 /**
  * Remove core update notifications for common users
- * @author Petr Kučera 
+ *
+ * @author Petr Kučera
  */
-function show_updated_only_to_admins()
-{
+function show_updated_only_to_admins() {
     if (!current_user_can('update_core')) {
-        remove_action( 'admin_notices', 'update_nag', 3 );
-remove_action( 'network_admin_notices', 'update_nag', 3 );
+        remove_action('admin_notices', 'update_nag', 3);
+        remove_action('network_admin_notices', 'update_nag', 3);
     }
 }
-add_action( 'admin_head', 'show_updated_only_to_admins', 1 );
+
+add_action('admin_head', 'show_updated_only_to_admins', 1);
 
 /**
  * Add multiple authors
  * using Co-Author Plus plugin
+ *
  * @author Petr Kučera
  */
 
-if ( function_exists( 'coauthors_posts_links' ) ) {
+if (function_exists('coauthors_posts_links')) {
     coauthors_posts_links();
 } else {
     the_author_posts_link();
@@ -275,19 +288,21 @@ if ( function_exists( 'coauthors_posts_links' ) ) {
 
 /**
  * Desible admin bar on website, because of caching
+ *
  * @author Petr Kucera, Matyas Koc
  */
 show_admin_bar(false);
 
 /**
  * Remove info about wordpress for better security
+ *
  * @author Petr Kucera, Matyas Koc
  */
 remove_action('wp_head', 'wp_generator');
-remove_action ('wp_head', 'rsd_link');
+remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wlwmanifest_link');
 
-if(is_user_logged_in()==false){
+if (is_user_logged_in() == false) {
 
     remove_action('wp_head', 'rest_output_link_wp_head', 10);
 
@@ -296,35 +311,38 @@ if(is_user_logged_in()==false){
 /**
  * Add more allowed mime types
  * DO NOT CHANGE WITHOUT APPROVAL
+ *
  * @author Matyas Koc
  */
 function additional_mime($mime_types) {
-  $mime_types['mcworld'] = 'application/zip';
-  $mime_types['zip'] = 'application/zip';
-  return $mime_types;
+    $mime_types['mcworld'] = 'application/zip';
+    $mime_types['zip'] = 'application/zip';
+    return $mime_types;
 }
 
-add_filter( 'upload_mimes', 'additional_mime', 99 );
+add_filter('upload_mimes', 'additional_mime', 99);
 
-function add_allow_upload_extension_exception( $types, $file, $filename, $mimes ) {
+function add_allow_upload_extension_exception($types, $file, $filename, $mimes) {
     // Do basic extension validation and MIME mapping
-    $wp_filetype = wp_check_filetype( $filename, $mimes );
-    $ext         = $wp_filetype['ext'];
-    $type        = $wp_filetype['type'];
-    if( in_array( $ext, array( 'mcworld', 'zip' ) ) ) { // Overrident extensions
+    $wp_filetype = wp_check_filetype($filename, $mimes);
+    $ext = $wp_filetype['ext'];
+    $type = $wp_filetype['type'];
+    if (in_array($ext, array('mcworld', 'zip'))) { // Overrident extensions
         $types['ext'] = $ext;
         $types['type'] = $type;
     }
     return $types;
 }
-add_filter( 'wp_check_filetype_and_ext', 'add_allow_upload_extension_exception', 99, 4 );
+
+add_filter('wp_check_filetype_and_ext', 'add_allow_upload_extension_exception', 99, 4);
 
 /**
  * Remove footer in admin-portal
+ *
  * @author Matyas Koc
  */
-add_filter( 'admin_footer_text', '__return_empty_string', 11 ); 
-add_filter( 'update_footer', '__return_empty_string', 11 );
+add_filter('admin_footer_text', '__return_empty_string', 11);
+add_filter('update_footer', '__return_empty_string', 11);
 add_filter('admin_footer_text', '__return_empty_string');
 
 ?>
